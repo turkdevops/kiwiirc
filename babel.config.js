899d6f2ba5ab15db6c478901137276a59b213cb0
Restyled.io <commits@restyled.io>
2021-02-02 11:53:15 +0000

    Restyled by prettier

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  presets : [
-    [
-      "@vue/cli-plugin-babel/preset",
-      {useBuiltIns : "entry", modules : "commonjs"},
-    ],
-  ],
-  plugins : [
-    [ "@babel/plugin-transform-runtime", {corejs : 3, useESModules : true} ],
-  ],
-  env : {
-    test : {
-      plugins : [
+    presets: [
         [
-          "istanbul",
-          {
-            exclude : [ "**/*.spec.js" ],
-          },
+            "@vue/cli-plugin-babel/preset",
+            { useBuiltIns: "entry", modules: "commonjs" },
         ],
-      ],
+    ],
+    plugins: [
+        ["@babel/plugin-transform-runtime", { corejs: 3, useESModules: true }],
+    ],
+    env: {
+        test: {
+            plugins: [
+                [
+                    "istanbul",
+                    {
+                        exclude: ["**/*.spec.js"],
+                    },
+                ],
+            ],
+        },
     },
-  },
 };
--- src/libs/TypingMiddleware.js
+++ src/libs/TypingMiddleware.js
@@ -6,81 +6,85 @@
  * Adds the +draft/typing IRCv3 spec to irc-framework
  */
 export default function typingMiddleware() {
-  return function middleware(client, rawEvents, parsedEvents) {
-    addFunctionsToClient(client);
-    rawEvents.use(theMiddleware);
-  };
+    return function middleware(client, rawEvents, parsedEvents) {
+        addFunctionsToClient(client);
+        rawEvents.use(theMiddleware);
+    };
 
-  function theMiddleware(command, message, rawLine, client, next) {
-    if (!(command === "TAGMSG" && message.tags["+draft/typing"]) &&
-        !(command === "PRIVMSG" && message.nick)) {
-      next();
-      return;
+    function theMiddleware(command, message, rawLine, client, next) {
+        if (
+            !(command === "TAGMSG" && message.tags["+draft/typing"]) &&
+            !(command === "PRIVMSG" && message.nick)
+        ) {
+            next();
+            return;
+        }
+
+        // if we are params[0] then the target is the sender (direct message)
+        let target =
+            message.params[0].toLowerCase() === client.user.nick.toLowerCase()
+                ? message.nick
+                : message.params[0];
+
+        // if its a privmsg without typing tag emit done
+        let status = message.tags["+draft/typing"] || "done";
+
+        client.emit("typing", {
+            target: target,
+            nick: message.nick,
+            ident: message.ident,
+            hostname: message.hostname,
+            status: status,
+        });
+
+        next();
     }
-
-    // if we are params[0] then the target is the sender (direct message)
-    let target =
-        message.params[0].toLowerCase() === client.user.nick.toLowerCase()
-            ? message.nick
-            : message.params[0];
-
-    // if its a privmsg without typing tag emit done
-    let status = message.tags["+draft/typing"] || "done";
-
-    client.emit("typing", {
-      target : target,
-      nick : message.nick,
-      ident : message.ident,
-      hostname : message.hostname,
-      status : status,
-    });
-
-    next();
-  }
 }
 
 function addFunctionsToClient(client) {
-  let typing = (client.typing = {});
-  let activeTyping = Object.create(null);
+    let typing = (client.typing = {});
+    let activeTyping = Object.create(null);
 
-  function isEnabled() { return client.network.cap.isEnabled("message-tags"); }
-
-  typing.start = function start(target) {
-    if (!isEnabled()) {
-      return;
+    function isEnabled() {
+        return client.network.cap.isEnabled("message-tags");
     }
 
-    let lastSentStatus = activeTyping[target.toLowerCase()];
-    if (lastSentStatus && lastSentStatus > Date.now() - 3000) {
-      return;
-    }
+    typing.start = function start(target) {
+        if (!isEnabled()) {
+            return;
+        }
 
-    activeTyping[target.toLowerCase()] = Date.now();
+        let lastSentStatus = activeTyping[target.toLowerCase()];
+        if (lastSentStatus && lastSentStatus > Date.now() - 3000) {
+            return;
+        }
 
-    let message = new client.Message("TAGMSG", target);
-    message.tags["+draft/typing"] = "active";
-    client.raw(message);
-  };
+        activeTyping[target.toLowerCase()] = Date.now();
 
-  typing.pause = function pause(target) {
-    if (!isEnabled()) {
-      return;
-    }
+        let message = new client.Message("TAGMSG", target);
+        message.tags["+draft/typing"] = "active";
+        client.raw(message);
+    };
 
-    let message = new client.Message("TAGMSG", target);
-    message.tags["+draft/typing"] = "paused";
-    client.raw(message);
-  };
+    typing.pause = function pause(target) {
+        if (!isEnabled()) {
+            return;
+        }
 
-  typing.stop = function stop(target) {
-    if (!isEnabled()) {
-      return;
-    }
+        let message = new client.Message("TAGMSG", target);
+        message.tags["+draft/typing"] = "paused";
+        client.raw(message);
+    };
 
-    let message = new client.Message("TAGMSG", target);
-    message.tags["+draft/typing"] = "done";
-    client.raw(message);
+    typing.stop = function stop(target) {
+        if (!isEnabled()) {
+            return;
+        }
 
-    delete activeTyping[target.toLowerCase()];
-  };
+        let message = new client.Message("TAGMSG", target);
+        message.tags["+draft/typing"] = "done";
+        client.raw(message);
+
+        delete activeTyping[target.toLowerCase()];
+    };
 }
