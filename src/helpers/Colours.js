967f15341b856068a1120d30e385994ffa400089
Restyled.io <commits@restyled.io>
2021-02-02 11:54:22 +0000

    Restyled by clang-format

--- src/helpers/Colours.js
+++ src/helpers/Colours.js
@@ -10,21 +10,17 @@
  * @return  {Object}          The RGB representation
  */
 export function hex2rgb(_hex) {
-    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    let hex = _hex.replace(
-        shorthandRegex,
-        (m, r, g, b) => r + r + g + g + b + b
-    );
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  let hex = _hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
 
-    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result
-        ? {
-              r: parseInt(result[1], 16),
-              g: parseInt(result[2], 16),
-              b: parseInt(result[3], 16),
-          }
-        : null;
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r : parseInt(result[1], 16),
+    g : parseInt(result[2], 16),
+    b : parseInt(result[3], 16),
+  }
+                : null;
 }
 
 /**
@@ -33,12 +29,9 @@ export function hex2rgb(_hex) {
  * @return {String}     Hex color string
  */
 export function rgb2hex(rgb) {
-    return (
-        "#" +
-        ["r", "g", "b"]
-            .map((key) => ("0" + rgb[key].toString(16)).slice(-2))
-            .join("")
-    );
+  return ("#" + [ "r", "g", "b" ]
+                    .map((key) => ("0" + rgb[key].toString(16)).slice(-2))
+                    .join(""));
 }
 
 /**
@@ -52,36 +45,36 @@ export function rgb2hex(rgb) {
  * @return  Object          HSL as h, s, and l keys
  */
 export function rgb2hsl(rgb) {
-    let r = rgb.r;
-    let g = rgb.g;
-    let b = rgb.b;
+  let r = rgb.r;
+  let g = rgb.g;
+  let b = rgb.b;
 
-    r /= 255;
-    g /= 255;
-    b /= 255;
+  r /= 255;
+  g /= 255;
+  b /= 255;
 
-    let max = Math.max(r, g, b);
-    let min = Math.min(r, g, b);
-    let h = 0;
-    let s = 0;
-    let l = (max + min) / 2;
+  let max = Math.max(r, g, b);
+  let min = Math.min(r, g, b);
+  let h = 0;
+  let s = 0;
+  let l = (max + min) / 2;
 
-    if (max === min) {
-        h = s = 0; // achromatic
-    } else {
-        let d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        if (max === r) {
-            h = (g - b) / d + (g < b ? 6 : 0);
-        } else if (max === g) {
-            h = (b - r) / d + 2;
-        } else if (max === b) {
-            h = (r - g) / d + 4;
-        }
-        h /= 6;
+  if (max === min) {
+    h = s = 0; // achromatic
+  } else {
+    let d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    if (max === r) {
+      h = (g - b) / d + (g < b ? 6 : 0);
+    } else if (max === g) {
+      h = (b - r) / d + 2;
+    } else if (max === b) {
+      h = (r - g) / d + 4;
     }
+    h /= 6;
+  }
 
-    return { h, s, l };
+  return {h, s, l};
 }
 
 /**
@@ -95,42 +88,47 @@ export function rgb2hsl(rgb) {
  * @return  Object          RGB as r, g, and b values
  */
 export function hsl2rgb(hsl) {
-    function hue2rgb(_p, _q, _t) {
-        let p = _p;
-        let q = _q;
-        let t = _t;
-        if (t < 0) t += 1;
-        if (t > 1) t -= 1;
-        if (t < 1 / 6) return p + (q - p) * 6 * t;
-        if (t < 1 / 2) return q;
-        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-        return p;
-    }
+  function hue2rgb(_p, _q, _t) {
+    let p = _p;
+    let q = _q;
+    let t = _t;
+    if (t < 0)
+      t += 1;
+    if (t > 1)
+      t -= 1;
+    if (t < 1 / 6)
+      return p + (q - p) * 6 * t;
+    if (t < 1 / 2)
+      return q;
+    if (t < 2 / 3)
+      return p + (q - p) * (2 / 3 - t) * 6;
+    return p;
+  }
 
-    let h = hsl.h;
-    let s = hsl.s;
-    let l = hsl.l;
-    let r = 0;
-    let g = 0;
-    let b = 0;
+  let h = hsl.h;
+  let s = hsl.s;
+  let l = hsl.l;
+  let r = 0;
+  let g = 0;
+  let b = 0;
 
-    if (s === 0) {
-        r = g = b = l; // achromatic
-    } else {
-        let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        let p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1 / 3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1 / 3);
-    }
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    let p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
 
-    return {
-        r: Math.round(r * 255),
-        g: Math.round(g * 255),
-        b: Math.round(b * 255),
-    };
+  return {
+    r : Math.round(r * 255),
+    g : Math.round(g * 255),
+    b : Math.round(b * 255),
+  };
 }
 
 export function rgb2rgbString(rgb) {
-    return "rgb(" + [rgb.r, rgb.g, rgb.b].join(",") + ")";
+  return "rgb(" + [ rgb.r, rgb.g, rgb.b ].join(",") + ")";
 }
